161c9b4a712b823f850274a3b08db4a187f2c07b
dragosmihaescu <147638603+dragosmihaescu@users.noreply.github.com>
2023-10-15 23:23:53 +0300

    Added while loop, implemented 5 turns with prompts

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 let computerPoints = 0;
 let playerPoints = 0;
 let result = 0;
+let turns = 0;
 
    const choices = ['Rock', 'Paper', 'Scissors'];
    const choice = choices[Math.floor(Math.random() * choices.length)]
    console.log(choice)
 
-
+ while(turns<=4) {
 
  function playRound(playerSelection, computerSelection) {
          if ((playerSelection === computerSelection )) {
@@ -18,10 +19,17 @@ let result = 0;
                 playerPoints++
                 console.log('Player won. You have ' + playerPoints + ' Points')
    }
+   turns++;
 }
-        
-        const playerSelection = prompt('Rock, Paper or Scissors?')
-        const computerSelection  = choice
-        console.log(playRound(playerSelection, computerSelection))
-        
+    const playerSelection = prompt('Rock, Paper or Scissors?')
+    const computerSelection  = choice
+    console.log(playRound(playerSelection, computerSelection))
+
+}
+
    
+
+
+
+
+
